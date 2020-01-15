b48e0cdacf58c980af21e36495e78f3c5edc62f4
jonathan <jonathan@sayurbox.com>
2020-01-15 11:07:27 +0700

    kafka

---
+++ mainApp/src/main/java/hello/MainController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
+    @PostMapping(path = "kirim")
+    public void kirimPesan() {
+        for (int i = 1; i <= 10; i++) {
+            this.template.send("pesan-saya", "Ini Pesan ke " + i);
+        }
+    }
+}
